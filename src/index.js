60dcb697187cf95308adcbe116456e0e0102f85d
kybavitalii <vitaliikyba@yahoo.com>
2021-08-11 21:43:40 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix || matrix.length === 0) {
+        return [];
+    }
+
+    function revers() {
+        for (let i = 0; i < matrix.length; i++) {
+            //reverse odd elements.
+            if (i % 2 === 1) {
+                matrix[i].reverse();
+            }
+        }
+        return matrix;
+    }
+    revers();
+    let arr = [];
+
+    matrix.reduce(function (accumulator, currentValue) {
+        return arr = accumulator.concat(currentValue);
+    });
+    return arr;
+};
